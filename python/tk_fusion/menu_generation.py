a16fff177e05b50706e56d3ab2b53edbdaa7ffbf
Orlando Esponda <orlando.esponda@gmail.com>
2025-05-23 15:29:49 -0600

    Fix unicode error (not needed anymore in Python 3)

--- python/tk_fusion/menu_generation.py
+++ python/tk_fusion/menu_generation.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2013 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 """
@@ -13,12 +13,12 @@ Menu handling for Natron
 
 """
 
-import tank
+import sgtk
 import sys
 import os
 import unicodedata
 
-from tank.platform.qt import QtGui, QtCore
+from sgtk.platform.qt import QtGui, QtCore
 
 
 __author__ = "Diego Garcia Huerta"
@@ -251,7 +251,7 @@ class AppCommand(object):
         app_instance = self.properties["app"]
         engine = app_instance.engine
 
-        for (app_instance_name, app_instance_obj) in engine.apps.items():
+        for app_instance_name, app_instance_obj in engine.apps.items():
             if app_instance_obj == app_instance:
                 # found our app!
                 return app_instance_name
@@ -265,10 +265,12 @@ class AppCommand(object):
         if "app" in self.properties:
             app = self.properties["app"]
             doc_url = app.documentation_url
+
+            # not needed in python 3
             # deal with nuke's inability to handle unicode. #fail
-            if doc_url.__class__ == unicode:
-                doc_url = unicodedata.normalize(
-                    'NFKD', doc_url).encode('ascii', 'ignore')
+            # if doc_url.__class__ == unicode:
+            #     doc_url = unicodedata.normalize(
+            #         'NFKD', doc_url).encode('ascii', 'ignore')
             return doc_url
 
         return None
